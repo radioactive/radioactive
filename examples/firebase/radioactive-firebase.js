f201d76612b92767585b6f91bf140bbbbe41c166
aldonline <aldo.bucchi@gmail.com>
2014-09-25 05:12:30 -0700

    += firebase examples

---
+++ examples/firebase/radioactive-firebase.js
@@ -0,0 +1,36 @@
+/*
+
+Adding radioactivity to any data producing framework is easy and can be achieved
+with a few lines of code.
+
+The following snipped of code is all that it takes to make Firebase fully radioactive.
+All 'Firebase' instances will now have an R(2) get() method.
+
+TODO: GC listeners
+
+Installation:
+To use this just make sure to add this after both Firebase and radioactive have been loaded
+
+Usage:
+
+var ref = new Firebase("https://.../")
+
+radioactive(function(){
+
+    console.log( ref.get().toUpperCase() )
+
+})
+
+*/
+(function(){
+    var cells = {};
+    Firebase.prototype.get = function(){
+        var url = this.toString();
+        var ref = this;
+        return ( cells[url] || (cells[url] = (function(){
+            var cell = radioactive( new radioactive.WaitSignal );
+            ref.on('value', function( snap ){ cell( snap.val() ) });
+            return cell;
+        })()))()
+    }
+})();
