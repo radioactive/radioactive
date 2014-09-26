196a8aed5d32d6bbe10a761fba29da238357edd6
aldonline <aldo.bucchi@gmail.com>
2014-09-26 00:13:32 -0700

    typescript definition ++

--- radioactive.d.ts
+++ radioactive.d.ts
@@ -1,80 +1,138 @@
-
+/**
+ * This typescript definition doubles as documentation.
+ * It is a good idea to read it through.
+ *
+ * Generating docs from this file is also a good idea.
+ * In fact, there's an open ticket. Feel like giving it a shot?
+ * https://github.com/radioactive/radioactive/issues/3
+ *
+ */
 declare module "radioactive" {
+
+
+    //////////// radioactive() --> radioactive.react()
+
+    /**
+     * Proxies to react() when called with one argument of type function
+     */
+    function r( expr: () => any ) : r.Stopper;
+
+    /**
+     * Proxies to react( expr, callback )
+     */
+    function r<T>( expr: () => T, callback: Callback<T> ) : r.Stopper;
+
+
+    //////////// radioactive() --> radioactive.cell()
 
     /**
      * Returns a cell initialized to undefined
      */
     function r(): r.Cell<any>;
 
-    /**
-     * Proxies to loop() when called with one argument of type function
-     */
-    function r( f: () => any ) : void;
 
     /**
      * Returns a cell initialized to @value when called with one argument that is not a function
+     * ( note that if the argument is a function this will not return a cell - it will forward to react() instead )
      */
     function r<T>( value: T ): r.Cell<T>;
 
 
+
     module r {
 
+        /**
+         * Returns true if we are currently within a reactive loop.
+         * ( we are inside a call to radioactive.react() or radioactive.once() ).
+         *
+         */
         function active(): boolean;
 
         /**
          * Request a Notifier.
-         * This method may return null if you are not running
-         * within a reactive function.
-         * NOTE: This function may return undefined.
-         *
-         * Why? Because Setting up Notifiers
-         * usually requires allocating resources ( timers, event listeners )
-         * that are expensive.
-         *
+         * This method may return null or undefined if we are not running within a reactive context.
+         * In other words: if radioactive.active() == false then this method will return null
          */
         function notifier(): Notifier;
 
+        /**
+         * Returns a forker.
+         *
+         * @see https://github.com/radioactive/radioactive/blob/master/test/radioactive.fork.tests.coffee
+         *
+         */
         function fork(): Forker;
 
+        /**
+         * Creates a Cell initialized to undefined
+         */
         function cell<T>(): Cell<T>;
 
-        function cell<T>( v:T ): Cell<T>;
+        /**
+         * Creates a cell initialized to @value
+         */
+        function cell<T>( value:T ): Cell<T>;
 
 
         /**
-         * Throws a StopSignal
+         * Throws a StopSignal.
+         * Use this to terminate a reactive loop.
+         *
+         * @see https://github.com/radioactive/radioactive/blob/master/test/radioactive.stop.tests.coffee
          */
         function stop(): void;
 
 
-        function react( f: () => any ): Stopper;
+        function react( expr: () => any ): Stopper;
 
-        function react<T>( f: () => T, cb: ( e?: Error, r: T ) => void ): Stopper;
+        function react<T>( expr: () => T, callback: Callback<T> ): Stopper;
 
-        function once<T>( f: () => T, cb: ( e?: Error, r: T ) => void ): Stopper;
+        function once<T>( expr: () => T, callback: Callback<T> ): Stopper;
 
-
-
-        function waiting( f: () => any ): boolean;
 
         /**
-         * Throws a WaitSignal
+         * Evaluates an expression and returns true if the expression throws a WaitSignal
+         * false otherwise.
+         *
+         * This function is R(1). Once the expression stops "waiting" it will reevaluate itself.
+         * You normally use this to show temporary "loading..." messages while R(2) expressions
+         * are in waiting state.
+         *
+         * @param expr
+         */
+        function waiting( expr: () => any ): boolean;
+
+        /**
+         * Throws a WaitSignal.
+         * Use this from a data producing expression to indicate
+         * there is work needed to be done.
+         * Note: It is your responsibility to notify when the work has completed.
+         * You notify by using the traditional radioactive.notifier() workflow.
          */
         function wait(): void;
 
-
+        /**
+         * Transforms an async function into a sync function that throws WaitSignals and caches results.
+         * The async function must follow the Node.js callback convention:
+         *
+         * function( error, result ){}
+         *
+         * @param async
+         */
         function syncify( async: Function ): Function;
 
+        /**
+         *
+         * @param opts
+         */
         function syncify( opts: SyncifyOpts ): Function;
 
         interface SyncifyOpts {
 
             /**
-             * Force expiration of cached results after N milliseconds.
-             * Defaults to 0 ( = infinite ).
-             * If you want to achieve polling functionality.
+             *
              */
-            ttl?: number ;
+            poll?: number ;
 
             /**
              *
@@ -84,6 +142,13 @@ declare module "radioactive" {
 
 
             /**
+             * Services are local by default ( as opposed to global ).
+             * A local service will receive its own cache that will last for one evaluation
+             * ( it is a bit more complicated than that in reality, but that is a fair approximation ).
+             * This is what you normally do when calling async functions. Each invocation keeps its own state.
+             *
+             * However. In many occasions it makes sense to have one global service.
+             * Specially when hitting a remote service.
              *
              */
             global?: boolean ;
@@ -93,13 +158,19 @@ declare module "radioactive" {
 
         }
 
-
-
-
-
+        /**
+         * Utility.
+         * Calling this function will return an R(2) function that knows how to echo() one value.
+         * R(0) ( but returns an R(2) function )
+         * @param delay
+         */
         function echo( delay: number = 1000 ): ( string ) => void;
 
-
+        /**
+         * Utility. Returns the current timestamp.
+         * @param interval
+         * R(1)
+         */
         function time( interval: number = 1000 ): number;
 
         /**
@@ -120,6 +191,12 @@ declare module "radioactive" {
             fire(): void
         }
 
+
+        interface Callback<T> {
+            ( error?: Error, result?: T ) : void ;
+        }
+
+
         interface Stopper {
             /**
              * Stop this watcher.
@@ -133,7 +210,7 @@ declare module "radioactive" {
              * May return null a few times before returning
              * the actual value.
              */
-            <T>( block: () => T ): T
+            <T>( expr: () => T ): T
 
             /**
              * Waits until all forked blocks of code are finished
@@ -157,13 +234,13 @@ declare module "radioactive" {
             ( ): T
             get( ): T
 
-            ( v: T ): void
-            set( v: T ): void
+            ( value: T ): void
+            set( value: T ): void
 
-            ( v: Error ): void
-            set( v: Error ): void
+            ( value: Error ): void
+            set( value: Error ): void
 
-            ( e?: Error, v?: T ): void
+            ( error?: Error, value?: T ): void
 
             /**
              * Whether someone is interested in knowing if our value changes.
@@ -171,11 +248,6 @@ declare module "radioactive" {
              */
             monitored():boolean
         }
-
-
-
-
-
 
     }
 
